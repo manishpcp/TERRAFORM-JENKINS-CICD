884cf63c7cab2b631af1c6ba06bbe6a55e95bc30
Manish Kumar <141416755+manishpcp@users.noreply.github.com>
2025-06-29 21:09:05 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "supermanishprojext1"
+    bucket         = "manishproject29062025"
     key            = "my-terraform-environment/main"
     region         = "us-east-1"
     dynamodb_table = "terraformmanishdb"
