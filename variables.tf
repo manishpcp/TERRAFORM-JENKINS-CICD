5f2beecdd4715de9c3e90b9a489624064cbc2b92
Manish Kumar <141416755+manishpcp@users.noreply.github.com>
2025-06-29 20:59:24 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,5 +17,5 @@ variable "ami_id" {
 variable "bucketname" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "terraformmanishaws"  #change Bucket name also
+  default     = "manishkumar-prod"  #change Bucket name also
 }
